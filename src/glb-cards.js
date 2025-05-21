0badbf5cea6256a3559e5247d643646daf6569c6
Franco <franbeltramella@hotmail.com>
2025-05-21 16:53:13 -0300

    gsap import

--- src/glb-cards.js
+++ src/glb-cards.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import gsap from 'gsap';
+
 
 function createScene(containerSelector, glbPath) {
   const container = document.querySelector(containerSelector);
