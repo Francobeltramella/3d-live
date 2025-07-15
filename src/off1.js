2beba23ead190cd70cf2dbd95d1d8e5d7187c866
Franco <franbeltramella@hotmail.com>
2025-07-14 23:16:05 -0300

    off 1 ice element 6 change

--- src/off1.js
+++ src/off1.js
@@ -15,6 +15,7 @@ const camera = new THREE.PerspectiveCamera(
   100
 );
 camera.position.set(3, 0, 0);
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
 renderer.setSize(container.clientWidth, container.clientHeight);
@@ -24,7 +25,7 @@ renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 container.appendChild(renderer.domElement);
 
 // Lights
-const light = new THREE.DirectionalLight(0xffffff, 2);
+const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(10, 10, 10);
 light.castShadow = true;
 light.shadow.mapSize.width = 2048;
@@ -32,7 +33,7 @@ light.shadow.mapSize.height = 2048;
 light.shadow.bias = -0.001;
 scene.add(light);
 
-const light2 = new THREE.DirectionalLight(0xffffff, 1);
+const light2 = new THREE.DirectionalLight(0xffffff, 2);
 light2.position.set(-10, 10, -10);
 light2.castShadow = true;
 scene.add(light2);
@@ -48,16 +49,14 @@ scene.add(hemiLight);
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
-/////controls.enableZoom = false;      // Desactiva el zoom con el scroll
-//controls.enableRotate = false;    // Desactiva la rotación con el mouse
-//controls.enablePan = false;
 
-const light4 = new THREE.DirectionalLight(0xffffff, 3);
+const light4 = new THREE.DirectionalLight(0xffffff, 2);
 light4.position.set(5, 5, 5);
 scene.add(light4);
 
-const ambient4 = new THREE.AmbientLight(0xffffff, 0.5);
+const ambient4 = new THREE.AmbientLight(0xffffff, 1.0);
 scene.add(ambient4);
 
 
@@ -69,30 +68,31 @@ const textureLoader = new THREE.TextureLoader();
 
 
 const diffuseMap   = textureLoader.load('https://cdn.prod.website-files.com/672b98669f7d5338576ee43f/6870599fa1a3837da3069885_686e146a92ab8954ee2aa224_op-thumbnail.webp');
-const normalMap    = textureLoader.load('https://cdn.prod.website-files.com/672b98669f7d5338576ee43f/6870828ccfd3153d7f29ff7c_Snow006_1K-JPG_NormalGL.jpg');
+const normalMap    = textureLoader.load('https://cdn.prod.website-files.com/672b98669f7d5338576ee43f/68756b3043280a2cfad92aa2_Snow005_1K-JPG_NormalGL.jpg');
 const roughnessMap = textureLoader.load('https://cdn.prod.website-files.com/672b98669f7d5338576ee43f/687082ab0af5fb0edfcc5c3a_Snow006_1K-JPG_Displacement.jpg');
 
 loader.load(
   "https://3dlive.netlify.app/tripo3.glb",
   (gltf) => {
     model = gltf.scene;
+
     // Mejora de materiales y sombras
     model.traverse((child) => {
         if (child.isMesh) {
             child.material.vertexColors = false;
             child.material = new THREE.MeshPhysicalMaterial({
-                color: new THREE.Color('#e8f6f7'),
-                transmission:0.6,
-                thickness: 1.0,
-                roughness: 0.40,
-                attenuationColor: new THREE.Color(0xaee7ff),
+                color: new THREE.Color('#E3F3FA'),
+                transmission:0.1,
+                thickness:1.5,
+                roughness: 4.95,
+                attenuationColor: new THREE.Color('#B5E8FF'),
                 attenuationDistance: 0.8,
-                metalness: 0.1,
-                ior: 1.51,
+                metalness: 0.00,
+                ior: 2.31,
                 transparent: true,
                 //opacity: 10,
                 envMap: diffuseMap,
-                envMapIntensity: 0.3,  // este valor es clave para que se vean los reflejos
+                envMapIntensity: 0.9,  // este valor es clave para que se vean los reflejos
                 side: THREE.DoubleSide,
                 normalMap: normalMap,
               });
